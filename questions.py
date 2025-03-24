9db8177986e2b847ae66711f2f014e9aa748236b
Valentin <schofflervalentin@gmail.com>
2025-03-24 15:58:38 -0300

    primera correccion

--- questions.py
+++ questions.py
@@ -1,4 +1,6 @@
 import random
+import sys
+
 # Preguntas para el juego
 questions = [
     "¿Qué función se usa para obtener la longitud de una cadena en Python?",
@@ -37,16 +39,27 @@ for _ in range(3):
 
     # El usuario tiene 2 intentos para responder correctamente
     for intento in range(2):
-        user_answer = int(input("Respuesta: ")) - 1
+        user_answer = input("Respuesta: ")
         # Se verifica si la respuesta es correcta
+
+        if user_answer.isdigit():
+            user_answer = int (user_answer) - 1
+            if user_answer < 0 or user_answer >= len(answers[question_index]):
+                print ("Respuesta no válida")
+                sys.exit(1)
+        else:
+            print("Respuesta no valida")
+            sys.exit(1)
+
         if user_answer == correct_answers_index[question_index]:
             print("¡Correcto!")
             break
-        else:
-            # Si el usuario no responde correctamente después de 2 intentos,
-            # se muestra la respuesta correcta
-            print("Incorrecto. La respuesta correcta es:")
-            print(answers[question_index] [correct_answers_index[question_index]])
+
+    else:
+        # Si el usuario no responde correctamente después de 2 intentos,
+        # se muestra la respuesta correcta
+        print("Incorrecto. La respuesta correcta es:")
+        print(answers[question_index] [correct_answers_index[question_index]])
             
-        # Se imprime un blanco al final de la pregunta
-        print()
+    # Se imprime un blanco al final de la pregunta
+    print()
